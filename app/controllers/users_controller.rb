3728aa45cde22a3605e68d53f7c607c1f9d7ddaa
etrinklein <etrinklein@gmail.com>
2021-05-31 19:11:02 +0000

    user page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,8 +12,9 @@ class UsersController < ApplicationController
     username = params.fetch("username")
 
     matching_users = User.where({ :username => username })
-
     @the_user = matching_users.at(0)
+
+    @list_of_photos = @the_user.photos.all
 
     render({ :template => "users/show.html.erb" })
 
