cacb5d6c66b4d4d3108016ea0dfd875007b3d952
etrinklein <etrinklein@gmail.com>
2021-05-31 16:06:15 +0000

    updated users page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
 
   def index
+    users = User.all
+    @list_of_users = users.order({ :username => :asc })
     render({ :template => "users/index.html.erb" })
   end
 
